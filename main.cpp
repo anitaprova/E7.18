7c5da2653d1817ee0fcd441226f5627f52c6c320
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-11-20 18:04:00 -0500

    done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+struct Point
+{
+        double x;
+        double y;
+};
+
+double distance(Point a, Point b) {
+        return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2));
+};
+
+struct Triangle {
+	Point x;
+	Point y;
+	Point z;
+};
+
+double perimeter(Triangle t){
+	return distance(t.x, t.y) + distance(t.y,t.z) + distance(t.z,t.x);
+}
+
+int main() {
+	Triangle tri = {{5,5}, {4,18}, {9,22}};
+	cout << perimeter(tri) << endl;
+	
+	return 0;
+}
